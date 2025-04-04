0fea4d4d2df6fc3432bed80d3e1c1248a716dc55
Sai Sneha <sneha@artpark.in>
2025-04-04 13:06:09 +0530

    fix(fetch): minor mods to support changing ecpds function

--- src/indra/fetch/__init__.py
+++ src/indra/fetch/__init__.py
@@ -1,7 +1,7 @@
 from indra.fetch.cds import app as cds_app
 from indra.fetch.cds import check_cds_credentials, fetch_and_upload_cds_data, last_date_of_cds_data, retrieve_data_from_cds
 from indra.fetch.ecpds import app as ecpds_app
-from indra.fetch.ecpds import fetch_and_upload_ecpds_data, last_date_of_ecpds_data, retrieve_data_from_ecpds
+from indra.fetch.ecpds import construct_ecpds_urls, last_date_of_ecpds_data
 from indra.fetch.imd import app as imd_app
 from indra.fetch.imd import clean_imd_data, retrieve_live_data_from_imd
 
@@ -9,9 +9,9 @@ __all__ = [
     "cds_app",
     "check_cds_credentials",
     "clean_imd_data",
+    "construct_ecpds_urls",
     "ecpds_app",
     "fetch_and_upload_cds_data",
-    "fetch_and_upload_ecpds_data",
     "imd_app",
     "last_date_of_cds_data",
     "last_date_of_ecpds_data",
