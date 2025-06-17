dc69dde2bbef81aa1715b5dde547c4c2ed72383d
swarna-an <swarna@artpark.in>
2025-06-17 17:44:20 +0530

    added back the clean_imd_func

--- src/indra/fetch/__init__.py
+++ src/indra/fetch/__init__.py
@@ -3,12 +3,14 @@ from indra.fetch.cds import check_cds_credentials, fetch_and_upload_cds_data, la
 from indra.fetch.ecpds import app as ecpds_app
 from indra.fetch.ecpds import construct_ecpds_urls, last_date_of_ecpds_data
 from indra.fetch.imd import app as imd_app
+from indra.fetch.imd import clean_imd_data
 from indra.fetch.imd_grid import app as imd_grid_app
 from indra.fetch.imd_grid import download_data_for_dates, download_gridded_data
 
 __all__ = [
     "cds_app",
     "check_cds_credentials",
+    "clean_imd_data",
     "construct_ecpds_urls",
     "download_data_for_dates",
     "download_gridded_data",
