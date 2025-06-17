2a5310de54ad41bfbf7d3536f2cdda8ca987dc9e
swarna-an <swarna@artpark.in>
2025-06-17 17:31:14 +0530

    added app for imd gridded data download and upload

--- src/indra/fetch/__init__.py
+++ src/indra/fetch/__init__.py
@@ -3,17 +3,21 @@ from indra.fetch.cds import check_cds_credentials, fetch_and_upload_cds_data, la
 from indra.fetch.ecpds import app as ecpds_app
 from indra.fetch.ecpds import construct_ecpds_urls, last_date_of_ecpds_data
 from indra.fetch.imd import app as imd_app
-from indra.fetch.imd import clean_imd_data
+from indra.fetch.imd_grid import app as imd_grid_app
+from indra.fetch.imd_grid import download_data_for_dates, download_gridded_data
 
 __all__ = [
     "cds_app",
     "check_cds_credentials",
-    "clean_imd_data",
     "construct_ecpds_urls",
+    "download_data_for_dates",
+    "download_gridded_data",
     "ecpds_app",
     "fetch_and_upload_cds_data",
     "imd_app",
+    "imd_grid_app",
     "last_date_of_cds_data",
     "last_date_of_ecpds_data",
     "retrieve_data_from_cds",
-    "retrieve_data_from_ecpds"]
+    "retrieve_data_from_ecpds",
+]
