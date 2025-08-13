96228e16cffa3dbeef0f610291e2c5cc62026124
Aishwarya Ramamurthy <aishwaryar@ARMacBookAir.local>
2025-08-13 15:28:02 +0530

    Updated imd_district_rainfall.py: Modified datacode in 2 statements to 'district_rainfall_'

--- src/indra/fetch/imd_district_rainfall.py
+++ src/indra/fetch/imd_district_rainfall.py
@@ -255,7 +255,7 @@ def main(
             raise ValueError(message)
     required_imd_params = ["url", "ds_id", "ds_name", "extension"]
     for datacode in params.keys():
-        if datacode.startswith("imd_district"):
+        if datacode.startswith("district_rainfall_"):
             for param in required_imd_params:
                 if param not in params[datacode]:
                     message = f"Missing required parameter: {param} for {datacode}"
@@ -301,7 +301,7 @@ def main(
             time = timecode.strftime("%H_%M_%S")
 
             # Iterate over both datacodes
-            datacodes = [datacode for datacode in params.keys() if datacode.startswith("imd_")]
+            datacodes = [datacode for datacode in params.keys() if datacode.startswith("district_rainfall_")]
             list_of_status_codes = {}
             for datacode in datacodes:
                 # Counting the number of files downloaded and uploaded
