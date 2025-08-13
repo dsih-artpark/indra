c1549ad007192f1c91573da27143f21f0142dade
Aishwarya Ramamurthy <aishwaryar@ARMacBookAir.local>
2025-08-13 15:05:58 +0530

    Updated imd_district_rainfall.py: Modified datacode to 'district_rainfall_'

--- src/indra/fetch/imd_district_rainfall.py
+++ src/indra/fetch/imd_district_rainfall.py
@@ -255,7 +255,7 @@ def main(
             raise ValueError(message)
     required_imd_params = ["url", "ds_id", "ds_name", "extension"]
     for datacode in params.keys():
-        if datacode.startswith("imd_district"):
+        if datacode.startswith("district_rainfall_"):
             for param in required_imd_params:
                 if param not in params[datacode]:
                     message = f"Missing required parameter: {param} for {datacode}"
